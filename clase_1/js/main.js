1f4dfbd0e0067cefc162757a18c6fd545ae1a298
Daniel Riverol <dannyriverol@hotmail.com>
2023-02-27 22:12:30 -0300

    Clase_1

---
+++ clase_1/js/main.js
@@ -0,0 +1,63 @@
+//Variables ES5 var nombre
+//declaracion
+var descriptionDeVariable = "Soy un a variable";
+var descriptionDeVariable = "Soy una nueva descripcion";
+
+//ES6
+let nombre = "Daniel";
+console.log(nombre);
+//dando otro valor
+nombre = "Juan";
+
+let apellido;
+//asignando
+apellido = "Gomez";
+
+//reasignando
+apellido = "Solano";
+let nombreCompleto;
+
+nombreCompleto = "Daniel Gomez";
+const PI = 3.14159;
+const IVA = 21;
+//IVA= 10.5;
+const E= 2.1212;
+
+
+//otros valores
+let usuario = "pepe";
+let pass = prompt("Crea tu pass");
+
+console.log(usuario+ " " + pass);
+pass = prompt("Actualiza tu pass");
+console.log(usuario+ " " + pass);
+let numero1 = 123;
+let numero2 = 345;
+
+//let resultado = (numero1 * numero2) + 90;
+let resultado = numero1 + numero2;
+//numero2= 890;
+
+let user = nombre + " " + apellido; //concatenación
+
+//metodo de salida
+/* console.log(nombre);
+console.log(PI);
+console.log(IVA);
+
+console.log(nombreCompleto);
+console.log(numero1);
+console.log(numero2);
+console.log(resultado);
+console.log(user);
+
+console.log(
+  "El resultado entre " + numero1 + " y " + numero2 + " es: " + resultado
+);
+
+//alert("El resultado entre " + numero1 + " y " + numero2 + " es: " + resultado);
+
+//metodo de entrada
+let alumno = prompt("Ingresa tu nombre completo");
+alert(alumno);
+ */
